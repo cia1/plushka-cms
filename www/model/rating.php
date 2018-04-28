42f469618140f2b5bc5e333e63224764160797a5
cia <seoutils@yandex.ru>
2018-04-28 15:57:53 +0300

    upload

---
+++ www/model/rating.php
@@ -0,0 +1,70 @@
+<?php
+/* Модуль rating. */
+
+abstract class rating {
+
+	protected static abstract function table();
+
+	protected static function alias() {
+		return static::table();
+	}
+
+	protected static function primaryKey() {
+		return 'id';
+	}
+
+	protected static function field() {
+			return 'rating';
+	}
+
+	//Возвращает часть SQL-запроса, секция SELECT
+	public static function sqlSelect() {
+		return 'rating.rating';
+	}
+
+	//Возвращает часть SQL-запроса, секция LEFT JOIN
+	public static function sqlLeftJoin() {
+		$db=core::db();
+		return ' LEFT JOIN rating ON rating.table='.$db->escape(static::table()).' AND rating.rowId='.static::alias().'.'.static::primaryKey().' AND rating.ip='.$db->escape(static::ip());
+	}
+
+	//Возвращает рейтинг для указанной записи таблицы БД
+	public static function getRating($rowId) {
+		$db=core::db();
+		return (float)$db->fetchValue('SELECT rating FROM rating WHERE table='.$db->escape($table).' AND rowId='.(int)$rowId.' AND ip='.$db->escape(self:_ip())));
+	}
+
+	public static function setRating($rowId,$value) {
+		$value=(int)$value;
+		if(!$value) return false;
+		$rating=(int)static::getRating($rowId);
+		if($value==$rating) return false;
+		$db=core::db();
+		if($rationg) {
+			$db->query('UPDATE rating SET value='.$value.' WHERE table='.$db->escape(static::table()).' AND rowId='.(int)$rowId.' AND ip='.$db->escape(self::_ip()));
+		} else {
+			$db->query('INSERT INTO rating (table,rowId,value,ip) VALUES ('.$db->escape(static::table()).','.(int)$rowId.','.$value.','.$db->escape(self::_ip()).')');
+		}
+		return $db->query('UPDATE '.static::table().' SET '.static::field().'='.static::field().'-'.$rating.'+'.$value);
+	}
+
+	private static function _ip() {
+		$client=(isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : null);
+		if(filter_var($client,FILTER_VALIDATE_IP)) return $client;
+		$forward=(isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : null);
+		if(filter_var($forward,FILTER_VALIDATE_IP)) return $forward;
+		$remote=(isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null);
+		return $remote;
+	}
+
+}
+
+/*
+CREATE TABLE rating (
+	userId INT UNSIGNED NOT NULL DEFAULT 0,
+	table CHAR(20) NOT NULL,
+	rowId INT UNSIGNED NOT NULL,
+	ip CHAR(15) NOT NULL,
+	rating VARCHAR(50)
+);
+*/
